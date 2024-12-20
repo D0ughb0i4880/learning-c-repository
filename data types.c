a555b187380da7e6f99f0e52d65a90caf82049b9
Mr. David <dbishop4880@outlook.com>
2024-12-20 12:07:02 -0500

    Update data types.c

--- data types.c
+++ data types.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+  // Create variables
+  int myNum = 15;               // Integer (whole number)
+  float myFloatNum = 8.75;     // Floating point number
+  char myLetter = 'L';         // Character
+  
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
